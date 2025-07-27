344c98772d963336b6da301b4b059e3514d7bd0b
Keydime <mdrefathchoudhury@gmail.com>
2025-07-27 00:20:40 -0400

    Utilized useState to like and dislike

--- Movie App/src/App.jsx
+++ Movie App/src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react"
+
 const Card = ({title}) => {
+
+    const [hasLiked, setHasLiked] = useState(false)
+
   return (
     <div className="card">
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "💜" : "🤍"}
+      </button>
     </div>
   )
 }
 
-
-
-
 const App = () => {
+
 
   return (
     <div className= "card-container">
